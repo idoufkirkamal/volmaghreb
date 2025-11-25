adb0356cac5f6f1e1efd407c5ff9ed75c766fe29
achraf ouzai <achrafouzai23@gmail.com>
2025-11-25 23:08:05 +0100

    commit

--- src/main/java/com/volmaghreb/reservation/controllers/AdminController.java
+++ src/main/java/com/volmaghreb/reservation/controllers/AdminController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Optional;
-//admin controller 
+// admin controller 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
